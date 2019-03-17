066cff8bf332f830ed32c24452550c4c383fbcd8
Mika Nokka <mika.nokka1@gmail.com>
2019-03-17 19:38:09 +0200

    Initial Commit

---
+++ src/CHelloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : CHelloWorld.c
+ Author      : mika.nokka1@gmail.com
+ Version     :
+ Copyright   : (c) Mika Nokka 2019
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
